1e6451919e1dec780581c2e91041ed4bad1c06d7
StoneEvens <steven930211@gmail.com>
2024-06-02 10:16:32 +0800

    Important Update

--- app/src/main/java/com/example/dbms/pages/CartFragment.java
+++ app/src/main/java/com/example/dbms/pages/CartFragment.java
@@ -1,16 +1,19 @@
 package com.example.dbms.pages;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.dbms.R;
 import com.example.dbms.cart.cartItem_adapter;
@@ -75,6 +78,19 @@ public class CartFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Button pathButton = getView().findViewById(R.id.startbutton);
+        pathButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getActivity().getSupportFragmentManager();
+                manager.beginTransaction()
+                        .replace(R.id.MainFragment, MapFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("Main")
+                        .commit();
+            }
+        });
 
         RecyclerView recyclerView = getView().findViewById(R.id.cartrecyclerview);
         ArrayList<cart_item> items = new ArrayList<cart_item>();
